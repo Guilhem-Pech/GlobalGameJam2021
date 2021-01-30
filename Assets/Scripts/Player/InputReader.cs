e89dc1d281f6e5edeceed8fbd42bfa653fcafa62
Guilhem PECH <guilhempech@gmail.com>
2021-01-30 12:24:31 +0100

    :sparkles: Add simple Pawn and PlayerController

---
+++ Assets/Scripts/Player/InputReader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
+    public class InputReader : ScriptableObject, GameInputs.IGameplayActions
+    {
+        private GameInputs _gameInputs;
+        private Vector2 _mousePos;
+        public UnityEvent<InputAction.CallbackContext> onLeftClick = new UnityEvent<InputAction.CallbackContext>(); 
+        
+        private void OnEnable()
+        {
+            _gameInputs = new GameInputs();
+            _gameInputs.Gameplay.SetCallbacks(this);
+            _gameInputs.Gameplay.Enable();
+            _gameInputs.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _gameInputs.Disable();
+        }
+
+        public Vector2 GetMousePosition()
+        {
+            return _mousePos;
+        }
+        
+        public void OnLeftClick(InputAction.CallbackContext context)
+        {
+            onLeftClick.Invoke(context);
+        }
+
+        public void OnMousePosition(InputAction.CallbackContext context)
+        {
+            _mousePos = context.ReadValue<Vector2>();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/VectorExt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class VectorExt
+    {
+        public static bool IsNearlyEqual(this Vector2 a, Vector2 b, float epsilon = 0.001f)
+        {
+            return (b - a).sqrMagnitude < epsilon * epsilon;
+        }
+    }
+}
