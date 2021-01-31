c8c5f3b2abbc277a100d7975b9f5c054f0191977
Guilhem PECH <guilhempech@gmail.com>
2021-01-31 16:41:34 +0100

    🔉 Integrate music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
     // Singleton 
     private static GameManager instance = null;
     public FModEvent ambient;
+    public FModEvent exploration;
+    public FModEvent docked;
     
     private void Awake(){
         if (instance == null){
@@ -14,10 +16,11 @@ public class GameManager : MonoBehaviour
         }
     }
 
-
     private void Start()
     {
         ambient?.Play();
+        docked?.Stop();
+        exploration?.Play();
     }
 
     public void EnterIslandMode()
@@ -25,11 +28,15 @@ public class GameManager : MonoBehaviour
         PlayerController player = PlayerController.Instance;
         player.enabled = false;
         player.directionCursor.Show(false);
+        docked?.Play();
+        exploration?.Stop();
 
     }
 
     public void EnterExploMode()
     {
+        docked?.Stop();
+        exploration?.Play();
         PlayerController player = PlayerController.Instance;
         player.enabled = true;
         player.directionCursor.Show(true);
